c4c089cbf20701ce726d5278d2bc6f4ed727f0ac
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-02-06 16:43:49 +0800

    NDK r28 has enabled 16KB page size support by default

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CGO_LDFLAGS="-Wl,-z,max-page-size=16384" gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags='-s -w -buildid=' . || exit 1
+gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags='-s -w -buildid=' . || exit 1
 rm -r libcore-sources.jar
 
 proj=../../app/libs
