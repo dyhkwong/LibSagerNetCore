382083e2be1166aeb7f0e850490667e27ce4040b
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-04-08 16:27:34 +0800

    Remove useless `-w` ldflags
    `-s` contains `-w`

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags='-s -w -buildid=' -tags='with_clash' . || exit 1
+gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags='-s -buildid=' -tags='with_clash' . || exit 1
 rm -r libcore-sources.jar
 
 proj=../../app/libs
